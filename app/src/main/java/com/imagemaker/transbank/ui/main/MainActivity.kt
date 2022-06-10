205391e9aa2248a3e32c658a9987a75cba2bfd92
Julian Martinez <julianmp0@gmail.com>
2022-06-09 21:48:50 -0500

    add: configure dagger hilt

--- app/src/main/java/com/imagemaker/transbank/ui/main/MainActivity.kt
+++ app/src/main/java/com/imagemaker/transbank/ui/main/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.imagemaker.transbank
+package com.imagemaker.transbank.ui.main
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -11,7 +11,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.imagemaker.transbank.ui.theme.TransBankTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
