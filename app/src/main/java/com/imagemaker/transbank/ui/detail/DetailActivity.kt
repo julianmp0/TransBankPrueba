899063c18ec0ad3304b36a49f581b9a18ed9fb8c
Julian Martinez <julianmp0@gmail.com>
2022-06-13 13:03:44 -0500

    add: show detail character

--- app/src/main/java/com/imagemaker/transbank/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/imagemaker/transbank/ui/detail/DetailActivity.kt
@@ -2,6 +2,8 @@ package com.imagemaker.transbank.ui.detail
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import coil.Coil
+import com.bumptech.glide.Glide
 import com.imagemaker.transbank.R
 import com.imagemaker.transbank.databinding.ActivityDetailBinding
 import com.imagemaker.transbank.model.models.Result
@@ -14,6 +16,19 @@ class DetailActivity : AppCompatActivity() {
         binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val character: Result? = intent.getSerializableExtra(Constans.EXTRAS.CHARACTER) as? Result
-        character
+
+        fillViews(character?:return)
+
+    }
+
+    private fun fillViews(character: Result) {
+        binding.apply {
+            Glide.with(this@DetailActivity).load(character.image).placeholder(R.drawable.default_image).into(ivCharacter)
+            tvNameCharacter.text = character.name
+            tvOriginCharacter.text =  "Origen: ${character.origin?.name?:"Desconocido"}"
+            tvLocationCharacter.text =  "Ubicación: ${character.location?.name?:"Desconocido"}"
+            tvStatusCharacter.text = "Estado: ${character.status}"
+            tvSpecieCharacter.text = "Especie: ${character.species}"
+        }
     }
 }
