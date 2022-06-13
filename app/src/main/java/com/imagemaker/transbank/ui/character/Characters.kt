756a1843a6097e1a2a28fe4ae54404f6b4dadca3
Julian Martinez <julianmp0@gmail.com>
2022-06-13 08:01:25 -0500

    add: show list of characters

--- app/src/main/java/com/imagemaker/transbank/ui/character/Characters.kt
+++ app/src/main/java/com/imagemaker/transbank/ui/character/Characters.kt
@@ -1,13 +1,39 @@
 package com.imagemaker.transbank.ui.character
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.imagemaker.transbank.model.models.CharacterModel
+import com.imagemaker.transbank.tools.extensions.visible
+import com.imagemaker.transbank.ui.theme.background
 import com.imagemaker.transbank.viewmodel.MainViewModel
 
 @Composable
 fun Characters(viewModel: MainViewModel) {
     val characters: CharacterModel? by viewModel.characterList.collectAsState(initial = null)
+    val isLoading: Boolean by viewModel.isLoading
+    ConstraintLayout {
+        val (progress, body) = createRefs()
+        HomeCharacters(
+            modifier = Modifier.background(background).constrainAs(body) {
+                top.linkTo(parent.top)
+            }.fillMaxHeight(),
+            characters = characters)
+        CircularProgressIndicator(
+            modifier = Modifier
+                .constrainAs(progress) {
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+                .visible(isLoading)
+        )
+    }
 
 }
---
+++ app/src/main/java/com/imagemaker/transbank/ui/character/HomeCharacters.kt
@@ -0,0 +1,133 @@
+package com.imagemaker.transbank.ui.character
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.imagemaker.transbank.model.models.CharacterModel
+import com.imagemaker.transbank.model.models.Result
+import com.imagemaker.transbank.ui.theme.shimmerHighLight
+import com.skydoves.landscapist.ShimmerParams
+import com.skydoves.landscapist.coil.CoilImage
+
+@Composable
+fun HomeCharacters(
+    modifier: Modifier, characters: CharacterModel?
+) {
+
+    if (characters == null)
+        return
+    LazyColumn(
+        modifier = modifier
+            .background(MaterialTheme.colors.background)
+    ) {
+        items(characters.results) { character ->
+            CharacterItem(character)
+        }
+    }
+
+}
+
+@Composable
+fun CharacterItem(character: Result) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        elevation = 4.dp,
+        shape = RoundedCornerShape(8.dp),
+        backgroundColor = MaterialTheme.colors.surface
+    ) {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .fillMaxWidth()
+        ) {
+            CoilImage(
+                imageModel = character.image,
+                modifier = Modifier
+                    .width(100.dp)
+                    .height(100.dp),
+                contentScale = ContentScale.Crop,
+                shimmerParams = ShimmerParams(
+                    baseColor = MaterialTheme.colors.background,
+                    highlightColor = shimmerHighLight,
+                    dropOff = 0.65f
+                ),
+            )
+            Spacer(modifier = Modifier.width(4.dp))
+            Column {
+
+                Text(
+                    text = character.name, style = TextStyle(
+                        fontSize = 20.sp,
+                        fontStyle = FontStyle.Normal,
+                        fontWeight = FontWeight.Bold
+                    )
+                )
+
+                Text(
+                    text = when (character.status) {
+                        "Alive" -> "Esta vivo"
+                        "Dead" -> "Esta muerto"
+                        else -> "Desconocido"
+                    },
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontStyle = FontStyle.Normal
+                    ),
+                    color = when (character.status) {
+                        "Alive" -> Color(74, 148, 0, 255)
+                        "Dead" -> Color.Red
+                        else -> Color.Blue
+                    }
+                )
+                Text(
+                    text = "Especie: ${character.species}",
+                    style = TextStyle(
+                        fontSize = 18.sp,
+                        fontStyle = FontStyle.Normal
+                    )
+                )
+            }
+
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun CharacterItemPreview() {
+    CharacterItem(
+        Result(
+            created = "2017-11-04T18:48:46.250Z",
+            episode = emptyList(),
+            gender = "Male",
+            id = 1,
+            image = "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+            location = null,
+            name = "Rick Sanchez",
+            origin = null,
+            species = "Human",
+            status = "Alive",
+            type = "",
+            url = "https://rickandmortyapi.com/api/character/1"
+        )
+    )
+}
+
