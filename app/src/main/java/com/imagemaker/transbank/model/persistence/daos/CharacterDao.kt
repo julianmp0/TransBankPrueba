4b84c50392b66cf35d8866da80df1dabd6e3b90c
Julian Martinez <julianmp0@gmail.com>
2022-06-13 15:10:04 -0500

    add: add room database

---
+++ app/src/main/java/com/imagemaker/transbank/model/persistence/daos/CharacterDao.kt
@@ -0,0 +1,16 @@
+package com.imagemaker.transbank.model.persistence.daos
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.imagemaker.transbank.model.persistence.entities.CharacterEntity
+
+@Dao
+interface CharacterDao {
+    @Query("SELECT * FROM character")
+    fun getAll(): List<CharacterEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(characters: List<CharacterEntity>)
+}
