4afd92cb3e336dd82634a9f2bc9077ea0570acc7
Julian Martinez <julianmp0@gmail.com>
2022-06-13 12:39:26 -0500

    add: enable viewBinding

--- app/src/main/java/com/imagemaker/transbank/ui/detail/DetailActivity.kt
+++ app/src/main/java/com/imagemaker/transbank/ui/detail/DetailActivity.kt
@@ -3,13 +3,16 @@ package com.imagemaker.transbank.ui.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.imagemaker.transbank.R
+import com.imagemaker.transbank.databinding.ActivityDetailBinding
 import com.imagemaker.transbank.model.models.Result
 import com.imagemaker.transbank.tools.Constans
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         val character: Result? = intent.getSerializableExtra(Constans.EXTRAS.CHARACTER) as? Result
         character
     }
