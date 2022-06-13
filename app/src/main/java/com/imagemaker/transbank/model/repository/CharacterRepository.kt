f44ec9574fbce797bfcb46f7e3889f2a59b817a7
Julian Martinez <julianmp0@gmail.com>
2022-06-13 15:12:51 -0500

    add: provide CharacterDao in CharacterRepository

--- app/src/main/java/com/imagemaker/transbank/model/repository/CharacterRepository.kt
+++ app/src/main/java/com/imagemaker/transbank/model/repository/CharacterRepository.kt
@@ -1,11 +1,13 @@
 package com.imagemaker.transbank.model.repository
 
 import com.imagemaker.transbank.model.network.RickAndMortyService
+import com.imagemaker.transbank.model.persistence.daos.CharacterDao
 import com.skydoves.sandwich.getOrNull
 import javax.inject.Inject
 
 class CharacterRepository @Inject constructor(
-    private val rickAndMortyService: RickAndMortyService
+    private val rickAndMortyService: RickAndMortyService,
+    private val characterDao: CharacterDao
 ) {
 
     suspend fun loadRickAndMortyCharacters(
